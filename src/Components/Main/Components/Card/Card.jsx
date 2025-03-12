d23eb2fc39803a9deb58468e6d7fb19dbf3811e3
Victor Ulloa <victoralfonsoulloa@gmail.com>
2025-03-12 00:09:38 -0600

    feat: create Card component and render card list
    Added Card.jsx component with props for name, link, and like status.
    Created a cards array in Main.jsx with mock data for development.
    Implemented dynamic rendering of Card components using map().

---
+++ src/Components/Main/Components/Card/Card.jsx
@@ -0,0 +1,30 @@
+import deleteButton from '../../../../images/delete-button.png'
+import likeButton from '../../../../images/like-button.png'
+
+
+export default function Card(props) {
+  const { name, link, isLiked } = props.card;
+
+  return (
+    <li className="card">
+      <img src={link} alt={name} className="card__image" />
+      <button className="card__delete-image">
+        <img
+          src={deleteButton}
+          alt="Delete button icon"
+          className="card__delete-image-icon"
+        />
+      </button>
+      <div className="card__caption">
+        <p className="card__caption_title">{name}</p>
+        <button className="card__caption_like">
+          <img
+            src={likeButton}
+            alt="Like button"
+            className="card__caption-like_icon"
+          />
+        </button>
+      </div>
+    </li>
+  );
+}
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,17 +2,19 @@ import { useState } from 'react';
 import addButton from '../../images/add-button.png';
 import editIcon from '../../images/edit_icon.svg';
 import editButton from '../../images/edit-button.png';
+import profilePicture from '../../images/linkedin-profile.jpeg';
 import Popup from './Components/Popup/Popup.jsx';
 import NewCard from './Components/Popup/Form/NewCard/NewCard.jsx';
 import EditAvatar from './Components/Popup/Form/EditAvatar/EditAvatar.jsx';
 import EditProfile from './Components/Popup/Form/EditProfile/EditProfile.jsx';
+import Card from './Components/Card/Card.jsx';
 
 export default function Main() {
   const [popup, setPopup] = useState(null);
 
   function handleOpenPopup(popup) {
     setPopup(popup);
-    console.log(popup)
+    console.log(popup);
   }
 
   function handleClosePopup() {
@@ -25,14 +27,35 @@ export default function Main() {
     children: <EditAvatar />,
   };
   const editProfilePopup = { title: 'Edit Profile', children: <EditProfile /> };
+  const cards = [
+    {
+      isLiked: false,
+      _id: '5d1f0611d321eb4bdcd707dd',
+      name: 'Yosemite Valley',
+      link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg',
+      owner: '5d1f0611d321eb4bdcd707dd',
+      createdAt: '2019-07-05T08:10:57.741Z',
+    },
+    {
+      isLiked: false,
+      _id: '5d1f064ed321eb4bdcd707de',
+      name: 'Lake Louise',
+      link: 'https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg',
+      owner: '5d1f0611d321eb4bdcd707dd',
+      createdAt: '2019-07-05T08:11:58.324Z',
+    },
+  ];
 
   return (
     <main className="main">
       <section className="profile">
         <div className="profile__container">
-          <div className="profile__image" onClick={() => handleOpenPopup(editAvatarPopup)}>
+          <div
+            className="profile__image"
+            onClick={() => handleOpenPopup(editAvatarPopup)}
+          >
             <img
-              src=""
+              src={profilePicture}
               alt="A profile picture"
               className="profile__image-user"
               id="profile__image-user"
@@ -46,7 +69,7 @@ export default function Main() {
 
           <div className="profile__bio">
             <div className="profile__bio-container">
-              <h1 className="profile__bio-name"></h1>
+              <h1 className="profile__bio-name">Victor Ulloa</h1>
               <button
                 className="profile__bio-open-popup"
                 id="profile__bio-open-popup"
@@ -59,18 +82,21 @@ export default function Main() {
                 />
               </button>
             </div>
-            <h2 className="profile__bio-description"></h2>
+            <h2 className="profile__bio-description">Software Engineer</h2>
           </div>
         </div>
-        <button className="profile__bio-add" onClick={() => handleOpenPopup(newCardPopup)}>
-          <img
-            src={addButton}
-            alt="Add button"
-            className="profile__add-icon"
-          />
+        <button
+          className="profile__bio-add"
+          onClick={() => handleOpenPopup(newCardPopup)}
+        >
+          <img src={addButton} alt="Add button" className="profile__add-icon" />
         </button>
       </section>
-      <section className="cards"></section>
+      <section className="cards"> 
+          {cards.map((card) => (
+            <Card key={card._id} card={card} />
+          ))}
+      </section>
       {popup && (
         <Popup onClose={handleClosePopup} title={popup.title}>
           {popup.children}
